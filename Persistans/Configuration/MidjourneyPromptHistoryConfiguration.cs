e85e499cc6705c94956f7db389faea842312c4a9
Marek Jaskuła <Marek.Jaskula1@gmail.com>
2025-06-04 11:06:50 +0200

    #Update configuration + Context + Conection String + Proiect clean

---
+++ Persistans/Configuration/MidjourneyPromptHistoryConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using Domain.Entities.MidjourneyPromtHistory;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using static Persistans.Constants.Constants;
+
+namespace Persistans.Configuration;
+
+public class MidjourneyPromptHistoryConfiguration : IEntityTypeConfiguration<MidjourneyPromptHistory>
+{
+    public void Configure(EntityTypeBuilder<MidjourneyPromptHistory> builder)
+    {
+        builder.ToTable("midjourney_prompt_history", schema: "public");
+        builder.HasKey(history => history.HistoryId);
+
+        builder
+            .Property(history => history.HistoryId)
+            .HasColumnName("history_id")
+            .HasColumnType(ColumnType.UniqueIdentifier);
+
+        builder
+            .Property(history => history.Prompt)
+            .HasColumnName("prompt")
+            .HasColumnType(ColumnType.NVarChar(1000))
+            .IsRequired();
+
+        builder
+            .Property(history => history.Version)
+            .HasColumnName("properties")
+            .HasColumnType(ColumnType.VarChar(7))
+            .IsRequired();
+
+        builder
+            .Property(history => history.CreatedOn)
+            .HasColumnName("created_on")
+            .HasColumnType(ColumnType.DateTimeOffset(7))
+            .HasDefaultValueSql("NOW()")
+            .IsRequired();
+
+        //builder
+        //    .HasOne(history => history.VersionMaster)
+        //    .WithMany(vm => vm.PromptHistories)
+        //    .HasForeignKey("version_master_id")
+        //    .IsRequired();
+    }
+}
---
+++ Persistans/Context/MidjourneyDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Persistans.Context;
+
+internal class MidjourneyDbContextFactory : IDesignTimeDbContextFactory<MidjourneyDbContext>
+{
+    public MidjourneyDbContext CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("connectionString.json")
+            .Build();
+
+        var connectionString = configuration.GetConnectionString("TestConnection");
+
+        var optionsBuilder = new DbContextOptionsBuilder<MidjourneyDbContext>();
+        optionsBuilder.UseNpgsql(connectionString);
+
+        return new MidjourneyDbContext(optionsBuilder.Options);
+    }
+}
