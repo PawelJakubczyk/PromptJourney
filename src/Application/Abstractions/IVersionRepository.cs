6edc10c3a5eb9da83754ad510e57e8f0867682ed
Marek Jaskuła <Marek.Jaskula1@gmail.com>
2025-07-07 15:59:28 +0200

    # newes repository

--- src/Application/Abstractions/IVersionRepository.cs
+++ src/Application/Abstractions/IVersionRepository.cs
@@ -10,6 +10,7 @@ public interface IVersionRepository
     Task<Result<List<MidjourneyVersionsBase>>> GetAllParametersByVersionMasterAsync(string version);
     Task<Result<bool>> CheckVersionExistsInVersionMasterAsync(string version);
     Task<Result<MidjourneyVersionsMaster>> GetMasterVersionByVersionAsync(string version);
+    Task<Result<MidjourneyVersionsMaster>> (string version, string? description);
 
     // Versions methods
     Task<Result<ParameterDetails>> AddParameterToVersionAsync(string version, string propertyName, string[] parameters, string? defaultValue, string? minValue, string? maxValue, string? description);
